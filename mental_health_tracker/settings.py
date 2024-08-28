6c65bc40c861a3bb2bb385486cbf753d2b9e56cc
beel <nabeelmuhammad1209@gmail.com>
2024-08-28 12:13:28 +0700

    adding new host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -121,4 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nabeel-muhammad-mentalhealthtracker.pbp.cs.ui.ac.id"]
+
