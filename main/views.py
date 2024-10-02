223b34d6b0b6ec005774f1b793edbd47ac1202c2
beel <nabeelmuhammad1209@gmail.com>
2024-10-02 13:51:39 +0700

    completing tutorial 5

--- main/views.py
+++ main/views.py
@@ -72,7 +72,6 @@ def login_user(request):
     form = AuthenticationForm()
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
-
     if form.is_valid():
         user = form.get_user()
         login(request, user)
